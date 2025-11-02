aa73785039ae7515434d9910d7b41b14abc076d9
Stefka Zhelyazkova <stefka.zheliazkova@gmail.com>
2025-11-02 12:12:39 +0200

    HelpHands Application (Initial commit)
    - Initial structure of the project

---
+++ src/test/java/app/HelpHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpHandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
